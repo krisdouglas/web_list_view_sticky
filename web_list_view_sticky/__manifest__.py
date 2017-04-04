934000618fbed639defcb534cc798ab664180e12
Kris Douglas <krisdouglas@gmail.com>
2017-04-04 17:03:55 +0100

    Initial layout adjustments
    Initial changes required for the module to be compatible with Odoo v10.

--- web_list_view_sticky/__manifest__.py
+++ web_list_view_sticky/__manifest__.py
@@ -1,16 +1,16 @@
 {
     'name': 'List View Fixed Table Header',
-    'version': '9.0.1',
+    'version': '10.0.1',
     'category': 'Tools',
     'sequence': 15,
     'summary': 'Web List View Fixed Table Header',
     'description': """
 Web List View Fixed Table Header
 =================================
-* Fixed (sticky) list view table header, very helpful when dealing with many record.
+* Fixed (sticky) list view table header, very helpful when dealing with many records.
 """,
-    'author': 'Andre Leander, upgraded to V9 by Stella Fredö in Sweden',
-    'website': 'leeshienwen@gmail.com',
+    'author': 'Andre Leander, upgraded to V9 by Stella Fredö in Sweeden, upgrades to V10 by Kris Douglas in England',
+    'website': 'krisdouglas@gmail.com',
     'depends': ['web','base'],
     'data': [
         'views/web_list_view_sticky.xml',
--- web_list_view_sticky/doc/changelog.rst
+++ web_list_view_sticky/doc/changelog.rst
@@ -3,6 +3,11 @@
 Changelog
 =========
 
+`Ver 10.0.1`
+---------
+ - Change manifest file for Odoo V10
+ - 
+
 `Ver 1.2`
 ---------
  - Fix Bug Horizontal Scroll when resize window
